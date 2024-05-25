a7359e97f69b57fcbc2c05bacf8ed104669f2a57
studiorabota <git@studiorabota.com>
2024-05-25 14:06:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## Installation
+
+To install the project on a Raspberry Pi, follow these steps:
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/wiggle-bin/wiggle-installer.git .
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd wiggle-installer
+    ```
+
+3. Make the `install.sh` script executable:
+
+    ```bash
+    chmod +x install.sh
+    ```
+
+4. Run the `install.sh` script:
+
+    ```bash
+    ./install.sh
+    ```
+
+    This script will handle the installation process and set up any necessary dependencies.
+
+5. Follow any additional instructions provided by the script.
+
+Once the installation is complete, you can proceed with using the project on your Raspberry Pi.
---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+folder="WiggleBin"
+
+# Function to create directories
+create_directories() {
+  echo "Creating directories..."
+  mkdir -p "~/$folder"
+  mkdir -p "~/$folder/packages"
+  mkdir -p "~/$folder/sensor-data"
+  mkdir -p "~/$folder/pictures"
+  mkdir -p "~/$folder/zip"
+  echo "Directories created at "~/$folder.""
+}
+
+# Function to create a virtual environment and install packages
+setup_virtualenv() {
+  echo "Setting up virtual environment..."
+  python3 -m venv "~/$folder/packages/venv"
+  source "~/$folder/packages/venv/bin/activate"
+  
+  # Packages to install
+  # packagesSystemWide="wiggle-camera wiggle-light"
+  # packages="wiggle-api wiggle-dashboard wiggle-settings wiggle-sensors wiggle-services"
+  packagesSystemWide="wiggle-camera"
+
+  # Install the packages
+  pip install --upgrade pip
+  pip install $packagesSystemWide --break-system-packages
+  # pip install $packages
+
+  wiggle-camera-install
+  # wiggle-light-install
+  # wiggle-api-install
+  # wiggle-gate-install
+  # wiggle-motion-install
+  # wiggle-settings-install
+  # wiggle-soil-temperature-install
+
+  echo "Packages installed: $packages"
+  deactivate
+}
+
+# Run the functions
+create_directories
+setup_virtualenv
+
+echo "Installation complete!"
